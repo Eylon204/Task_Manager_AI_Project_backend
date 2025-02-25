205b65bcb90504dbf9f50a4188e9fa47b8cda2d7
Eylon204 <Eylon204@Gmail.com>
2025-02-25 22:09:03 +0200

    Resolved merge conflict in security.py

--- app/core/security.py
+++ app/core/security.py
@@ -2,30 +2,6 @@ from datetime import datetime, timedelta
 from typing import Union
 import jwt
 from passlib.context import CryptContext
-<<<<<<< HEAD
-from app.core.config import Settings
-
-settings = Settings()
-# Password hashing
-pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
-
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hased_password: str) -> bool:
-    return pwd_context.verify(plain_password, hased_password)
-
-# JWT Token handling
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta 
-    else:
-        expire = datetime.utcnow() + timedelta (minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-        to_encode.update({"exp":expire})
-        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
-        return encoded_jwt
-=======
 from app.core.config import settings
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
@@ -63,4 +39,3 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         return email
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
->>>>>>> 07eae6ae (Added new backend files and updated repository)
