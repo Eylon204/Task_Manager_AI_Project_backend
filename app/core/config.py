a791137321a44380e4d09b8d1110df93349ce5f8
Eylon204 <Eylon204@Gmail.com>
2025-02-25 22:11:07 +0200

    Resolved merge conflict in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # טוען משתני סביבה מקובץ .env
+load_dotenv()
 
 class Settings:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://eylon204:eylon2041@cluster1.wx9lw.mongodb.net/?retryWrites=true&w=majority")
-<<<<<<< HEAD
-    MONGO_DB_NAME = os.getenv("MON׳O_DB_NAME", "TaskManagerDB")
-=======
-    MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "TaskManagerDB")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "supersecretkey")
-    JWT_ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 60
->>>>>>> 07eae6ae (Added new backend files and updated repository)
+    PROJECT_NAME: str = "Task Manager AI"
+    VERSION: str = "1.0"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "your_secret_key")
+    JWT_ALGORITHM: str = "HS256"
+    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017/task_manager")
 
 settings = Settings()
